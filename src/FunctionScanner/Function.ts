fa18ffc357aa6ff52863d0c76a3d0ae825e6ec5d
Soulike <soulike@qq.com>
2020-11-21 15:54:59 +0800

    完成 MemberExpressionProcessor

--- src/FunctionScanner/Function.ts
+++ src/FunctionScanner/Function.ts
@@ -1,4 +1,81 @@
+import * as ESTree from 'estree';
+import * as esprima from 'esprima';
+
 export function throwRangeIsUndefinedException(): never
 {
     throw new Error('the key "range" should not be undefined. check esprima config');
+}
+
+/**
+ * @description e.g. obj.a['b'].c => [c, b.c, a.b.c, obj.a.b.c], ignore other attribute values like Symbol
+ * */
+export function getNamesFromChainedMemberExpression(expression: ESTree.MemberExpression): string[]
+{
+    const nameChain = getNamesFromChainedMemberExpressionHelper(expression);
+    const nameChainSplit = nameChain.split('.');
+    const names: string[] = [];
+    for (let i = nameChainSplit.length - 1; i >= 0; i--)
+    {
+        const id = nameChainSplit[i];
+        if (names.length === 0)
+        {
+            names.push(id);
+        }
+        else
+        {
+            const lastName = names[names.length - 1];
+            names.push(`${id}.${lastName}`);
+        }
+    }
+    return names;
+}
+
+/**
+ * @description e.g. obj.a['b'].c[0].d => obj.a.b.c.0.d
+ * */
+function getNamesFromChainedMemberExpressionHelper(expression: ESTree.MemberExpression | ESTree.Identifier | ESTree.Literal): string
+{
+    if (expression.type === esprima.Syntax.MemberExpression)
+    {
+        if (expression.property.type === esprima.Syntax.Identifier || expression.property.type === esprima.Syntax.Literal)
+        {
+            let name: string;
+            if (expression.property.type === esprima.Syntax.Identifier)
+            {
+                name = expression.property.name;
+            }
+            else
+            {
+                name = String(expression.property.value);
+            }
+            if (expression.object.type === esprima.Syntax.MemberExpression
+                || expression.object.type === esprima.Syntax.Identifier
+                || expression.object.type === esprima.Syntax.Literal)
+            {
+                const objectChainName = getNamesFromChainedMemberExpressionHelper(expression.object);
+                if (objectChainName.length === 0)
+                {
+                    return '';
+                }
+                return `${objectChainName}.${name}`;
+            }
+            else
+            {
+                return '';
+            }
+        }
+        else    // ignore other types
+        {
+            return '';
+        }
+    }
+    else if (expression.type === esprima.Syntax.Identifier)
+    {
+        const {name} = expression;
+        return name;
+    }
+    else
+    {
+        return '';
+    }
 }
---
+++ src/FunctionScanner/RightProcessor/MemberExpressionProcessor.ts
@@ -0,0 +1,37 @@
+import {NodeProcessor} from '../Interface/NodeProcessor';
+import * as ESTree from 'estree';
+import {FunctionInfo} from '../../DataClass/FunctionInfo';
+import {getNamesFromChainedMemberExpression} from '../Function';
+import {IdentifierProcessor} from './IdentifierProcessor';
+import * as esprima from 'esprima';
+
+export class MemberExpressionProcessor implements NodeProcessor
+{
+    private readonly leftIdentifierNames: Set<string>;
+    private readonly memberExpression: Readonly<ESTree.MemberExpression>;
+    private readonly knownFunctionInfos: Readonly<Readonly<FunctionInfo>[]>;
+
+    constructor(leftIdentifierNames: Set<string>, memberExpression: Readonly<ESTree.MemberExpression>, knownFunctionInfos: Readonly<Readonly<FunctionInfo>[]>)
+    {
+        this.leftIdentifierNames = leftIdentifierNames;
+        this.memberExpression = memberExpression;
+        this.knownFunctionInfos = knownFunctionInfos;
+    }
+
+    /**
+     * @return return null since the function just finds FunctionInfo that has the same function name and add it to name set
+     * */
+    getPartialFunctionInfo(): null
+    {
+        const names = getNamesFromChainedMemberExpression(this.memberExpression);
+        // just think that each chain name is a identifier
+        names.forEach(name =>
+        {
+            const identifierProcessor = new IdentifierProcessor(this.leftIdentifierNames,
+                {type: esprima.Syntax.Identifier, name},
+                this.knownFunctionInfos);
+            identifierProcessor.getPartialFunctionInfo();
+        });
+        return null;
+    }
+}
