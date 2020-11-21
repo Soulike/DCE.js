cc71e7881c381a1c10e541b7932936300af41f41
Soulike <soulike@qq.com>
2020-11-21 10:41:29 +0800

    完成 FunctionDeclarationProcessor

---
+++ src/FunctionScanner/FunctionDeclarationProcessor.ts
@@ -0,0 +1,37 @@
+import * as ESTree from 'estree';
+import {FunctionInfo} from '../DataClass/FunctionInfo';
+import {throwRangeIsUndefinedException} from './Function';
+
+export class FunctionDeclarationProcessor
+{
+    private readonly functionDeclaration: Readonly<ESTree.FunctionDeclaration>;
+
+    constructor(functionDeclaration: Readonly<ESTree.FunctionDeclaration>)
+    {
+        this.functionDeclaration = functionDeclaration;
+    }
+
+    public getPartialFunctionInfo(): Pick<FunctionInfo, 'bodyStartIndex' | 'bodyEndIndex' | 'name'>
+    {
+        const {functionDeclaration} = this;
+
+        const name = new Set<string>();
+        if (functionDeclaration.id === null)
+        {
+            throw new Error('Currently not supporting anonymous function declaration');
+        }
+        name.add(functionDeclaration.id.name);
+
+        const {body: {range}} = functionDeclaration;
+        if (range === undefined)
+        {
+            throwRangeIsUndefinedException();
+        }
+        const [bodyStartIndex, bodyEndIndex] = [range[0] + 1, range[1] - 1];
+        return {
+            bodyStartIndex,
+            bodyEndIndex,
+            name,
+        };
+    }
+}
