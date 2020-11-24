ff8b905d1d42f34bb6ce027432d69c0b2b286012
Soulike <soulike@qq.com>
2020-11-24 17:30:08 +0800

    添加基础数据结构

---
+++ src/JavaScriptProcessor/Class/Range.ts
@@ -0,0 +1,11 @@
+export class Range
+{
+    public readonly startIndex: number;
+    public readonly endIndex: number;
+
+    constructor(startIndex: number, endIndex: number)
+    {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+    }
+}
---
+++ src/JavaScriptProcessor/Class/ReplaceInfo.ts
@@ -0,0 +1,15 @@
+export class ReplaceInfo
+{
+    public readonly range: Readonly<Range>;
+    public readonly code: string;
+
+    /**
+     * @param range - the range of replaced code
+     * @param code - new code after replace
+     * */
+    constructor(range: Readonly<Range>, code: string)
+    {
+        this.range = range;
+        this.code = code;
+    }
+}
