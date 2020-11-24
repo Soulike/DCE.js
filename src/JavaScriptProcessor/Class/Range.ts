5c22bba10abc5ab05a0f41b27940c349e1c2e526
Soulike <soulike@qq.com>
2020-11-24 17:34:51 +0800

    完善基础数据结构

--- src/JavaScriptProcessor/Class/Range.ts
+++ src/JavaScriptProcessor/Class/Range.ts
@@ -5,6 +5,10 @@ export class Range
 
     constructor(startIndex: number, endIndex: number)
     {
+        if (startIndex < 0 || endIndex < 0)
+        {
+            throw new Error('index should not be negative.');
+        }
         this.startIndex = startIndex;
         this.endIndex = endIndex;
     }
--- src/JavaScriptProcessor/Interface/JavaScriptProcessor.ts
+++ src/JavaScriptProcessor/Interface/JavaScriptProcessor.ts
@@ -2,5 +2,5 @@ import {ReplaceInfo} from '../Class/ReplaceInfo';
 
 export interface JavaScriptProcessor
 {
-    getReplaceInfo(): ReplaceInfo;
+    getReplaceInfo(): ReplaceInfo | null;
 }
