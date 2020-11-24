d183e4cced7bfb147967002910f5e5214aacc387
Soulike <soulike@qq.com>
2020-11-24 16:23:55 +0800

    FunctionScanner 的 ASTFilter 改为注入形式

--- src/FunctionScanner/FunctionScanner.ts
+++ src/FunctionScanner/FunctionScanner.ts
@@ -2,22 +2,26 @@ import {ScriptFile} from '../DataClass/ScriptFile';
 import {FunctionInfo} from '../DataClass/FunctionInfo';
 import fse from 'fs-extra';
 import {FunctionScannerForCode} from './FunctionScannerForCode';
+import {ASTNodeFilter as IASTNodeFilter} from './EsprimaWrapper';
+import {ASTNodeFilter} from './ASTNodeFilter';
 
 export class FunctionScanner
 {
     private readonly scriptFile: Readonly<ScriptFile>;
     private readonly encoding: BufferEncoding;
+    private readonly astNodeFilter: IASTNodeFilter;
 
-    constructor(scriptFile: Readonly<ScriptFile>, encoding: BufferEncoding = 'utf-8')
+    constructor(scriptFile: Readonly<ScriptFile>, astNodeFilter: IASTNodeFilter = new ASTNodeFilter(), encoding: BufferEncoding = 'utf-8')
     {
         this.scriptFile = scriptFile;
+        this.astNodeFilter = astNodeFilter;
         this.encoding = encoding;
     }
 
     public async getFunctionInfos(): Promise<FunctionInfo[]>
     {
         const code = await fse.promises.readFile(this.scriptFile.filePath, this.encoding);
-        const functionScannerForCode = new FunctionScannerForCode(code);
+        const functionScannerForCode = new FunctionScannerForCode(code, this.astNodeFilter);
         const partialFunctionInfos = await functionScannerForCode.getPartialFunctionInfos();
         return partialFunctionInfos.map(
             ({name, startIndex, endIndex, bodyStartIndex, bodyEndIndex}) =>
