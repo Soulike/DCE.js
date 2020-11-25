eb66dcc308fca8a62c54a7fc1a48844514a8a57b
Soulike <soulike@qq.com>
2020-11-25 10:48:38 +0800

    修改 FunctionInfo 类
    - 修改构造函数
    - 添加 isGlobal() 方法
    - 修改判定相等条件

--- src/DataClass/FunctionInfo.ts
+++ src/DataClass/FunctionInfo.ts
@@ -10,28 +10,43 @@ export class FunctionInfo
 
     constructor(scriptFile: Readonly<ScriptFile> | null, startIndex: number | null, endIndex: number | null, bodyStartIndex: number | null, bodyEndIndex: number | null)
     {
-        this.scriptFile = scriptFile;
-        this.startIndex = startIndex;
-        this.endIndex = endIndex;
-        this.bodyStartIndex = bodyStartIndex;
-        this.bodyEndIndex = bodyEndIndex;
+        if (scriptFile === null || startIndex === null || endIndex === null || bodyStartIndex === null || bodyEndIndex === null)
+        {
+            this.scriptFile = null;
+            this.startIndex = null;
+            this.endIndex = null;
+            this.bodyStartIndex = null;
+            this.bodyEndIndex = null;
+        }
+        else
+        {
+            this.scriptFile = scriptFile;
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
+            this.bodyStartIndex = bodyStartIndex;
+            this.bodyEndIndex = bodyEndIndex;
+        }
     }
 
     public equals(functionInfo: FunctionInfo): boolean
     {
         const {scriptFile, startIndex, endIndex} = functionInfo;
-        if (scriptFile === null && startIndex === null && endIndex === null
-            && this.scriptFile === null && this.startIndex === null && this.endIndex === null)
+        if (scriptFile === null || this.scriptFile === null)
         {
-            return true;
-        }
-        if (scriptFile === null || startIndex === null || endIndex === null
-            || this.scriptFile === null || this.startIndex === null || this.endIndex === null)
-        {
-            return false;
+            return this.isGlobal() && functionInfo.isGlobal();
         }
         return scriptFile.equals(this.scriptFile)
             && startIndex === this.startIndex
             && endIndex === this.endIndex;
+    }
+
+    public isGlobal(): boolean
+    {
+        const {scriptFile, endIndex, startIndex, bodyEndIndex, bodyStartIndex} = this;
+        return scriptFile === null
+            || endIndex === null
+            || startIndex === null
+            || bodyStartIndex === null
+            || bodyEndIndex === null;
     }
 }
