2544869f36b69cfb96238dee31d59a8c99bebee3
Soulike <soulike@qq.com>
2020-11-30 17:49:25 +0800

    初步完成 RowColumnToIndexConverter

---
+++ src/RowColumnToIndexConverter/RowColumnToIndexConverter.ts
@@ -0,0 +1,56 @@
+import {ScriptFile} from '../DataClass/ScriptFile';
+import fse from 'fs-extra';
+
+/**
+ * @description the scriptFile passed to a RowColumnToIndexConverter should not be modified.
+ * If the file is modified, you must create a new RowColumnToIndexConverter.
+ * */
+export class RowColumnToIndexConverter
+{
+    private readonly scriptFile: Readonly<ScriptFile>;
+    private fileContentLines: string[] | null;
+    private readonly encoding: BufferEncoding;
+
+    constructor(scriptFile: Readonly<ScriptFile>, encoding: BufferEncoding = 'utf-8')
+    {
+        this.scriptFile = scriptFile;
+        this.encoding = encoding;
+        this.fileContentLines = null;
+    }
+
+    public async getIndex(rowNumber: number, columnNumber: number): Promise<number>
+    {
+        if (this.fileContentLines === null)
+        {
+            await this.readFileContentLines();
+        }
+        // fileContent should not be null ofter readFileContentLines()
+        const fileContentLines = this.fileContentLines as string[];
+        if (rowNumber > fileContentLines.length)
+        {
+            throw new Error(`non-existent row number ${rowNumber} for file ${this.scriptFile.filePath}`);
+        }
+        if (columnNumber > fileContentLines[rowNumber - 1].length + 1)   // +1 for '\n' consumed in split()
+        {
+            throw new Error(`non-existent column number ${columnNumber} for file ${this.scriptFile.filePath}`);
+        }
+        let index = 0;
+        for (let i = 0; i < rowNumber - 1; i++)
+        {
+            index += fileContentLines[i].length + 1;  // +1 for '\n' consumed in split()
+        }
+        index += columnNumber;
+        return index;
+    }
+
+    private async readFileContentLines()
+    {
+        const {filePath} = this.scriptFile;
+        const fileContent = await fse.promises.readFile(filePath, this.encoding);
+        if (fileContent.length === 0)
+        {
+            throw new Error(`file ${filePath} is empty`);
+        }
+        this.fileContentLines = fileContent.split('\n');
+    }
+}
