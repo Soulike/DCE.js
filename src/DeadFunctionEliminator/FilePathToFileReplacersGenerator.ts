0fe331560dbd2e5aba6ed15eda78a6559b57e657
Soulike <soulike@qq.com>
2020-12-12 16:35:25 +0800

    删除 FilePathToFileReplacersGenerator 中无用注释

--- src/DeadFunctionEliminator/FilePathToFileReplacersGenerator.ts
+++ src/DeadFunctionEliminator/FilePathToFileReplacersGenerator.ts
@@ -43,7 +43,6 @@ export class FilePathToFileReplacersGenerator
                 }
                 else
                 {
-                    // TODO: prevent overlap
                     replaceInfos.push(replaceInfo);
                 }
             }
